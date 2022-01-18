61b8fa6c770ac4a92e6d31fb655674212fc925d0
RahulBorade94 <boraderahul94@gmail.com>
2022-01-18 20:07:04 +0530

    MODIFILED: Java Genrics Method Implementation

--- src/com/bridgelabz/genrics/PrintArray.java
+++ src/com/bridgelabz/genrics/PrintArray.java
@@ -2,6 +2,16 @@ package com.bridgelabz.genrics;
 
 public class PrintArray {
 
+    public static <E> void toPrint (E [] inputArray){
+        for (E element: inputArray) {
+            System.out.printf("%s", element);
+
+        }
+        System.out.println();
+
+    }
+
+
     public static void main(String[] args) {
         Integer [] intArray = {1,2,3,4,5};
         Double [] doubleArray = {1.1,2.2,3.3,4.4,5.5};
@@ -10,31 +20,6 @@ public class PrintArray {
         PrintArray.toPrint(doubleArray);
         PrintArray.toPrint(charArray);
 
-
-    }
-
-    public static void toPrint(Integer[] inputArray){
-        for (int element: inputArray) {
-            System.out.printf("%s", element);
-
-        }
-        System.out.println();
-
-    }
-    public static void toPrint(Double[] inputArray){
-        for (double element: inputArray) {
-            System.out.printf("", element);
-
-        }
-        System.out.println();
-
-    }
-    public static void toPrint(Character[] inputArray){
-        for (char element: inputArray) {
-            System.out.printf("%s", element);
-
-        }
-        System.out.println();
 
     }
 
