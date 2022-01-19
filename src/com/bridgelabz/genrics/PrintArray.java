2ba8222ba2d6b7e9ed8a4804f395e2026d353d5b
RahulBorade94 <boraderahul94@gmail.com>
2022-01-19 14:50:49 +0530

    MODIFILED: Genric Method define 3 object

--- src/com/bridgelabz/genrics/PrintArray.java
+++ src/com/bridgelabz/genrics/PrintArray.java
@@ -1,47 +1,26 @@
 package com.bridgelabz.genrics;
 
 public class PrintArray {
+    public <T extends Comparable<T>> T compareTo(T x, T y, T z) {
+        T max = x;
 
-    public static Integer compareTo(Integer x,Integer y,Integer z){
-        Integer max=x;
-        if (y.compareTo(max)> 0){
-            max=y;
+        if (y.compareTo(max) > 0) {
+            max = y;
         }
-        if (z.compareTo(max)> 0){
-            max=z;
-        }
-              return max;
-
-    }
-
-
-    public static Float compareTo(Float x,Float y,Float z){
-        Float max=x;
-        if (y.compareTo(max)> 0){
-            max=y;
-        }
-        if (z.compareTo(max)> 0){
-            max=z;
+        if (z.compareTo(max) > 0) {
+            max = z;
         }
         return max;
-
     }
 
-    public static String compareTo(String x,String y,String z){
-        String max=x;
-        if (y.compareTo(max)> 0){
-            max=y;
-        }
-        if (z.compareTo(max)> 0){
-            max=z;
-        }
-        return max;
 
-    }
     public static void main(String[] args) {
-        System.out.println("Maximum from 3 Integer Object : "+compareTo(30,100,56));
-        System.out.println("Maximum from 3 Float Object : "+compareTo(50.0F,20.0F,30.0F));
-        System.out.println("Maximum from 3 String Object : "+compareTo("Apple","Peach","Banana"));
+        PrintArray integerComp = new PrintArray();
+        System.out.println("Maximum from 3 Integer Object : "+integerComp.compareTo(30,100,56));
+        PrintArray floatComp = new PrintArray();
+        System.out.println("Maximum from 3 Float Object : "+floatComp.compareTo(50.0F,20.0F,30.0F));
+        PrintArray stringComp = new PrintArray();
+        System.out.println("Maximum from 3 String Object : "+stringComp.compareTo("Apple","Peach","Banana"));
 
     }
 
