6ced845abcda76794c6014fa7b39caec7c18610b
RahulBorade94 <boraderahul94@gmail.com>
2022-01-19 12:18:49 +0530

    MODIFILED: find maximum 3 float obaject

--- src/com/bridgelabz/genrics/PrintArray.java
+++ src/com/bridgelabz/genrics/PrintArray.java
@@ -14,8 +14,21 @@ public class PrintArray {
 
     }
 
+
+    public static Float compareTo(Float x,Float y,Float z){
+        Float max=x;
+        if (y.compareTo(max)> 0){
+            max=y;
+        }
+        if (z.compareTo(max)> 0){
+            max=z;
+        }
+        return max;
+
+    }
     public static void main(String[] args) {
         System.out.println("Maximum from 3 Integer Object : "+compareTo(30,100,56));
+        System.out.println("Maximum from 3 Float Object : "+compareTo(50.0F,20.0F,30.0F));
 
     }
 
