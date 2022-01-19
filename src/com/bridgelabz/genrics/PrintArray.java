5e9d52b610934358f34975a42fc19f012aee3ab0
RahulBorade94 <boraderahul94@gmail.com>
2022-01-19 14:26:49 +0530

    MODIFILED: Find maximum from 3 string

--- src/com/bridgelabz/genrics/PrintArray.java
+++ src/com/bridgelabz/genrics/PrintArray.java
@@ -26,9 +26,22 @@ public class PrintArray {
         return max;
 
     }
+
+    public static String compareTo(String x,String y,String z){
+        String max=x;
+        if (y.compareTo(max)> 0){
+            max=y;
+        }
+        if (z.compareTo(max)> 0){
+            max=z;
+        }
+        return max;
+
+    }
     public static void main(String[] args) {
         System.out.println("Maximum from 3 Integer Object : "+compareTo(30,100,56));
         System.out.println("Maximum from 3 Float Object : "+compareTo(50.0F,20.0F,30.0F));
+        System.out.println("Maximum from 3 String Object : "+compareTo("Apple","Peach","Banana"));
 
     }
 
